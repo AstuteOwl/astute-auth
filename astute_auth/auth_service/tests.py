70bdfed55a0b7c43c499f26f7409eebdbe895600
James Strassburg <jstrassburg@gmail.com>
2015-06-16 21:38:44 -0500

    Flipping example tests over to rest framework testing

--- astute_auth/auth_service/tests.py
+++ astute_auth/auth_service/tests.py
@@ -1,12 +1,12 @@
-from django.test import TestCase
-
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Create your tests here.
-class AuthTestCase(TestCase):
+class AuthTestCase(APITestCase):
     def test_auth_200(self):
         resp = self.client.get('/auth/')
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
 
-    def test_no_auth_400(self):
+    def test_no_auth_401(self):
         resp = self.client.get('/noauth/')
-        self.assertEqual(resp.status_code, 401)
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
