b16ee34eb59247990c70694e0f10da6754e14b7d
James Strassburg <jstrassburg@gmail.com>
2015-06-17 22:25:33 -0500

    Refactor with a JsonCorsResponse class that will automatically alow all origins

--- astute_auth/auth_service/views.py
+++ astute_auth/auth_service/views.py
@@ -3,28 +3,24 @@ import datetime
 from astute_auth.auth_service.request_serializers import TokenRequestSerializer
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from jwt import generate_jwt
 from astute_auth import settings
 
 
-class JSONResponse(HttpResponse):
+class JsonCorsResponse(Response):
 	"""
-    An HttpResponse that renders its content into JSON.
-    """
+	A Response that renders its content into JSON and allows all origins.
+	"""
 
-	def __init__(self, data, **kwargs):
-		content = JSONRenderer().render(data)
+	def __init__(self, **kwargs):
 		kwargs['content_type'] = 'application/json'
-		super(JSONResponse, self).__init__(content, **kwargs)
-
-	# Access-Control-Allow-Origin
+		kwargs['headers'] = {"Access-Control-Allow-Origin": "*"}
+		super(JsonCorsResponse, self).__init__(**kwargs)
 
 
 @api_view(['POST'])
@@ -34,7 +30,7 @@ def token(request):
 	token_serializer = TokenRequestSerializer(data=data)
 
 	if not token_serializer.is_valid():
-		return HttpResponse(status=status.HTTP_400_BAD_REQUEST, headers={"Access-Control-Allow-Origin": "*"})
+		return JsonCorsResponse(status=status.HTTP_400_BAD_REQUEST)
 	
 	email = token_serializer.validated_data['email']
 	password = token_serializer.validated_data['password']
@@ -42,13 +38,13 @@ def token(request):
 	if User.objects.filter(username=email).exists():
 		user = authenticate(username=email, password=password)
 		if user is None:
-			return Response(status=status.HTTP_401_UNAUTHORIZED, headers={"Access-Control-Allow-Origin": "*"})
+			return JsonCorsResponse(status=status.HTTP_401_UNAUTHORIZED)
 		else:
 			claims = {'email': email, 'permissions': list(user.get_all_permissions())}
 			jwt = generate_jwt(claims, settings.HMAC_SECRET, 'HS512', datetime.timedelta(days=365))
-			response = {'token':jwt}
-			return Response(response, status=status.HTTP_201_CREATED, headers={"Access-Control-Allow-Origin": "*"})
+			response_data = {'token':jwt}
+			return JsonCorsResponse(data=response_data, status=status.HTTP_201_CREATED)
 	else:
 		user = User.objects.create_user(username=email, email=email, password=password)
 		# TODO: send verification email
-		return Response(status=status.HTTP_202_ACCEPTED, headers={"Access-Control-Allow-Origin": "*"})
+		return JsonCorsResponse(status=status.HTTP_202_ACCEPTED)
