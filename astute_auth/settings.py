ce137e02ac75663ea139a282546f7dbeba3d17d0
ryanco <ryangcolwell@gmail.com>
2015-07-12 22:08:01 -0500

    More changes to support static content locally and on heroku.

--- astute_auth/settings.py
+++ astute_auth/settings.py
@@ -102,9 +102,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ()
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
